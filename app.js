fcbb7512af4468a7c6d06c31a134fd5e06f74da2
zeted0410 <148453706+zeted0410@users.noreply.github.com>
2024-02-17 13:38:00 +1030

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/main.html'); 
+  res.sendFile(__dirname + '/index.html'); 
 });
 
 app.use(express.static('public')); 
